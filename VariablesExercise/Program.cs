cf6c7d742fa6b0c02b675da8328c36ed88c831de
Robert Schofield <rschofield1992@gmail.com>
2024-03-21 13:28:36 -0400

    inital commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿// Declare and initalize variables
+string name = "john";
+int age = 31;
+char middleInital = 'D';
+bool isOver18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 29.99m;
+
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"age: {age}");
+Console.WriteLine($"Middle Inital: {middleInital}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
